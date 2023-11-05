839a4c17ca40fa32c451eb306f5cb0caa0861e61
Burhan Mohammad Sarfraz <36539269+Hvaheterdu@users.noreply.github.com>
2023-11-05 01:28:37 +0100

    Update InvalidListIndex.java

--- src/main/java/doublylinkedlist/InvalidListIndex.java
+++ src/main/java/doublylinkedlist/InvalidListIndex.java
@@ -1,7 +1,6 @@
 package doublylinkedlist;
 
 class InvalidListIndex extends RuntimeException {
-
     InvalidListIndex(int index) {
         super("Invalid index: " + index + ". Cannot add element to this index.");
     }
