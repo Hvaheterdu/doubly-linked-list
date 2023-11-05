da19a61965f7b2c4f5e6290f3a00054d75ac2b7b
Burhan Mohammad Sarfraz <36539269+Hvaheterdu@users.noreply.github.com>
2023-11-05 01:18:14 +0100

    Update Stack.java

--- src/main/java/doublylinkedlist/Stack.java
+++ src/main/java/doublylinkedlist/Stack.java
@@ -2,9 +2,6 @@ package doublylinkedlist;
 
 class Stack<T> extends LinkedList<T> {
 
-    /**
-     * Inserting element to the top of the stack
-     */
     public void insertOn(T x) {
         if (super.contains(x)) {
             return;
@@ -13,9 +10,6 @@ class Stack<T> extends LinkedList<T> {
         }
     }
 
-    /**
-     * Remove the last element added from the stack
-     */
     public T takeOff() {
         int size = super.size();
         return super.removePos(size - 1);
