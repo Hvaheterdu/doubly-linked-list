c765953016f7c63d1e52f7c921b3cc064d91b811
Burhan Mohammad Sarfraz <burhan.sarfraz@bouvet.no>
2024-02-02 10:01:01 +0100

    Newline between class def and constructor

--- src/main/java/doublylinkedlist/InvalidListIndex.java
+++ src/main/java/doublylinkedlist/InvalidListIndex.java
@@ -1,6 +1,7 @@
 package doublylinkedlist;
 
 class InvalidListIndex extends RuntimeException {
+
     InvalidListIndex(int index) {
         super("Invalid index: " + index + ". Cannot add element to this index.");
     }
